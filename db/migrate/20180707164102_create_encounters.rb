240f031caf18287c5192014b730721175a756869
EricaWhyte <erica_whyte@hotmail.com>
2018-07-07 12:58:14 -0400

    initial commit

---
+++ db/migrate/20180707164102_create_encounters.rb
@@ -0,0 +1,14 @@
+class CreateEncounters < ActiveRecord::Migration
+  def change
+    create_table :encounters do |t|
+      t.string :visit_number
+      t.datetime :admitted_at
+      t.datetime :discharged_at
+      t.string :location
+      t.string :room
+      t.string :bed
+
+      t.timestamps null: false
+    end
+  end
+end
---
+++ db/migrate/20180707164516_create_patients.rb
@@ -0,0 +1,14 @@
+class CreatePatients < ActiveRecord::Migration
+  def change
+    create_table :patients do |t|
+      t.text :first_name
+      t.text :middle_name
+      t.text :last_name
+      t.number :weight
+      t.number :height
+      t.string :mrn
+
+      t.timestamps null: false
+    end
+  end
+end
